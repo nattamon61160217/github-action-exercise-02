c62efd0e612a28a4db48551365d5f7f8d01b28b9
Nattamon Jaroensirisombat <61160217@go.buu.ac.th>
2021-09-21 20:10:05 +0700

    HW1

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const port = process.env.port || 8080
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('Hello world!!!')
+})
 app.get('/users', (req, res) => {
     res.send('Nattamon Jaroensirisombat')
 })
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -6,3 +6,9 @@ describe('GET /', () => {
         request(app).get('/').expect('Hello world!!!', done)
     })
 })
+
+describe('GET /user', () => {
+    it('responds with "Nattamon Jaroensirisombat"', (done) => {
+        request(app).get('/').expect('Nattamon Jaroensirisombat', done)
+    })
+})
