53e6a4de69d5dea6ce371b25fbad68ad97d458af
Nattamon Jaroensirisombat <61160217@go.buu.ac.th>
2021-09-21 20:06:52 +0700

    HW

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const port = process.env.port || 8080
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello world!!!')
+app.get('/users', (req, res) => {
+    res.send('Nattamon Jaroensirisombat')
 })
 
 app.listen(port, () => {
-    console.log('The app is up and running!')
+    console.log('No user in the system')
 })
 module.exports = app
